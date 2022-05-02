a30794c97f287ac25d37863b0be586ef2c1a59cb
eyemotif <yepicness@gmail.com>
2022-05-02 15:11:37 -0400

    Add project files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/i,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(mp3|wav|ogg)$/i,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+}
