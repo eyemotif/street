8e8285b870cb0d164ea3c2bdf7d334df08f039ae
eyemotif <yepicness@gmail.com>
2022-05-02 16:22:27 -0400

    Finish up last commit

---
+++ src/components.d.ts
@@ -0,0 +1,13 @@
+// audio components
+declare module '*.mp3' {
+    const path: string
+    export default path
+}
+declare module '*.wav' {
+    const path: string
+    export default path
+}
+declare module '*.ogg' {
+    const path: string
+    export default path
+}
